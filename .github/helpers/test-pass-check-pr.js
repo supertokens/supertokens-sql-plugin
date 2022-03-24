8c2bec48442ff4d35114f8926eef1c4679269fdb
Nemi Shah <nemishah1212@gmail.com>
2022-03-24 17:32:49 +0530

    Change test check script

--- .github/helpers/test-pass-check-pr.js
+++ .github/helpers/test-pass-check-pr.js
@@ -1,7 +1,6 @@
 const { default: axios } = require("axios");
 
-const linkToJob =
-  "https://github.com/supertokens/supertokens-sql-plugin/actions/workflows/tests.yml";
+const linkToJob = "https://github.com/supertokens/supertokens-sql-plugin/actions/workflows/tests.yml";
 const testJobName = "Run tests";
 
 let thisRunId = process.env.RUN_ID;
@@ -13,96 +12,79 @@ thisRunId = thisRunId.trim();
 let startTime = Date.now();
 
 function doJob() {
-  console.log("Checking job status...");
+    console.log("Checking job status...");
 
-  const currentOwnerAndRepoString = process.env.REPO;
-  const sourceRepoWithOwnerString = `${process.env.SOURCE_OWNER}/${
-    currentOwnerAndRepoString.split("/")[1]
-  }`;
+    const currentOwnerAndRepoString = process.env.REPO;
+    const sourceRepoWithOwnerString = `${process.env.SOURCE_OWNER}/${
+      currentOwnerAndRepoString.split("/")[1]
+    }`;
 
-  console.log("SOURCE REPO", sourceRepoWithOwnerString);
-
-  axios
-    .get(
-      `https://api.github.com/repos/${sourceRepoWithOwnerString}/actions/runs?branch=${process.env.BRANCH}`,
-      {
+    axios.get(`https://api.github.com/repos/${sourceRepoWithOwnerString}/actions/runs?branch=${process.env.BRANCH}`, {
         headers: {
-          Authorization: `token ${gitHubToken}`,
-        },
-      }
-    )
-    .then(async (result) => {
-      let data = result.data;
-      let currentSHA = undefined;
-
-      console.log(JSON.stringify(data));
-
-      data.workflow_runs.forEach((run) => {
-        if (run.id + "" === thisRunId) {
-          currentSHA = run.head_sha;
+            'Authorization': `token ${gitHubToken}`
         }
-      });
+    }).then(async result => {
+        let data = result.data;
+        let currentSHA = process.env.CURRENT_SHA;
+        console.log("CURRENT SHA:", currentSHA)
 
-      let foundActiveJob = false;
-      let success = false;
-      let failed = false;
-      if (currentSHA !== undefined) {
-        for (let i = 0; i < data.workflow_runs.length; i++) {
-          let run = data.workflow_runs[i];
-          if (run.head_sha === currentSHA) {
-            if (run.name === testJobName) {
-              if (run.conclusion === "success") {
-                success = true;
-              } else if (run.conclusion === "failure") {
-                failed = true;
-              }
-              if (run.status === "in_progress") {
-                foundActiveJob = true;
-              }
+        // data.workflow_runs.forEach(run => {
+        //     if ((run.id + "") === thisRunId) {
+        //         currentSHA = run.head_sha;
+        //     }
+        // });
+
+        let foundActiveJob = false;
+        let success = false;
+        let failed = false;
+        if (currentSHA !== undefined) {
+            for (let i = 0; i < data.workflow_runs.length; i++) {
+                let run = data.workflow_runs[i];
+                if (run.head_sha === currentSHA) {
+                    if (run.name === testJobName) {
+                        if (run.conclusion === "success") {
+                            success = true;
+                        } else if (run.conclusion === "failure") {
+                            failed = true;
+                        }
+                        if (run.status === "in_progress") {
+                            foundActiveJob = true;
+                        }
+                    }
+                }
             }
-          }
         }
-      }
 
-      if (success) {
-        console.log("Success!");
-        process.exit(0);
-        return;
-      }
+        if (success) {
+            console.log("Success!");
+            process.exit(0);
+            return;
+        }
 
-      if (foundActiveJob) {
-        console.log("Waiting for job to finish...");
-        return setTimeout(doJob, 30000); // try again after 30 seconds.
-      }
+        if (foundActiveJob) {
+            console.log("Waiting for job to finish...");
+            return setTimeout(doJob, 30000) // try again after 30 seconds.
+        }
 
-      if (failed) {
-        console.log(
-          "Test job failed. Exiting... Please rerun this job manually when you run the test job again..."
-        );
-        process.exit(1);
-        return;
-      }
+        if (failed) {
+            console.log("Test job failed. Exiting... Please rerun this job manually when you run the test job again...");
+            process.exit(1);
+            return;
+        }
 
-      console.log(
-        'You need to trigger the "' +
-          testJobName +
-          '" github action and make this job succeed'
-      );
-      console.log("You can find the github action here: " + linkToJob);
-      if (Date.now() - startTime > 1.5 * 60 * 1000) {
-        console.log(
-          "Test job not started after waiting for 1.5 mins... Exiting, please rerun this job manually."
-        );
-        process.exit(1);
-      } else {
-        setTimeout(doJob, 30000);
-      }
+        console.log("You need to trigger the \"" + testJobName + "\" github action and make this job succeed");
+        console.log("You can find the github action here: " + linkToJob);
+        if ((Date.now() - startTime) > (3 * 60 * 1000)) {
+            console.log("Test job not started after waiting for 3 mins... Exiting, please rerun this job manually.");
+            process.exit(1);
+        } else {
+            setTimeout(doJob, 30000);
+        }
+    }).catch((e) => {
+        console.log(e);
+        console.log("Error thrown.. waiting for 1 min and trying again.");
+        setTimeout(doJob, 60000) // try again after 1 min.
     })
-    .catch((e) => {
-      console.log(e);
-      console.log("Error thrown.. waiting for 1 min and trying again.");
-      setTimeout(doJob, 60000); // try again after 1 min.
-    });
 }
 
 doJob();
