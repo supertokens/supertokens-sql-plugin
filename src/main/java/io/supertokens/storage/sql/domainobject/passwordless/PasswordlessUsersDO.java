f70c81410f4fa813c3992b8fb8c53497fa7006e1
anugrahsinghal <anugrahsinghal1@gmail.com>
2022-07-21 01:16:40 +0530

    feat: adds entity for Passwordless Queries

---
+++ src/main/java/io/supertokens/storage/sql/domainobject/passwordless/PasswordlessUsersDO.java
@@ -0,0 +1,71 @@
+/*
+ *    Copyright (c) 2022, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ *    This software is licensed under the Apache License, Version 2.0 (the
+ *    "License") as published by the Apache Software Foundation.
+ *
+ *    You may not use this file except in compliance with the License. You may
+ *    obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ */
+
+package io.supertokens.storage.sql.domainobject.passwordless;
+
+import io.supertokens.storage.sql.domainobject.PrimaryKeyFetchable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "passwordless_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordlessUsersDO extends PrimaryKeyFetchable {
+
+    @Id
+    @Column(length = 36)
+    private String user_id;
+
+    @Column(length = 256, unique = true)
+    private String email;
+
+    @Column(length = 256, unique = true)
+    private String phone_number;
+
+    @Column(nullable = false)
+    private long time_joined;
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof PasswordlessUsersDO) {
+            PasswordlessUsersDO otherDO = (PasswordlessUsersDO) other;
+            return otherDO.getUser_id().equals(this.getUser_id());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getUser_id().hashCode();
+    }
+
+    @Override
+    public Serializable getPrimaryKey() {
+        return user_id;
+    }
+
+}
--- src/main/java/io/supertokens/storage/sql/hibernate/HibernateUtils.java
+++ src/main/java/io/supertokens/storage/sql/hibernate/HibernateUtils.java
@@ -22,8 +22,9 @@ import io.supertokens.storage.sql.domainobject.emailpassword.EmailPasswordUsersD
 import io.supertokens.storage.sql.domainobject.emailpassword.PasswordResetTokensDO;
 import io.supertokens.storage.sql.domainobject.general.AllAuthRecipeUsersDO;
 import io.supertokens.storage.sql.domainobject.general.KeyValueDO;
-import io.supertokens.storage.sql.domainobject.session.SessionAccessTokenSigningKeysDO;
-import io.supertokens.storage.sql.domainobject.session.SessionInfoDO;
+import io.supertokens.storage.sql.domainobject.passwordless.PasswordlessCodesDO;
+import io.supertokens.storage.sql.domainobject.passwordless.PasswordlessDevicesDO;
+import io.supertokens.storage.sql.domainobject.passwordless.PasswordlessUsersDO;
 import io.supertokens.storage.sql.domainobject.thirdparty.ThirdPartyUsersDO;
 import io.supertokens.storage.sql.domainobject.useridmapping.UserIdMappingDO;
 import io.supertokens.storage.sql.domainobject.usermetadata.UserMetadataDO;
@@ -65,7 +66,9 @@ public class HibernateUtils {
     private static Class<?>[] entities() {
         return new Class[] { KeyValueDO.class, AllAuthRecipeUsersDO.class, EmailPasswordUsersDO.class,
                 PasswordResetTokensDO.class, ThirdPartyUsersDO.class, UserMetadataDO.class, SessionInfoDO.class,
-                SessionAccessTokenSigningKeysDO.class, UserIdMappingDO.class };
+                SessionAccessTokenSigningKeysDO.class,
+                UserIdMappingDO.class, PasswordlessCodesDO.class, PasswordlessDevicesDO.class,
+                PasswordlessUsersDO.class };
     }
 
     @NotNull
